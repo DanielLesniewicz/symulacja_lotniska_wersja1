a7c13492175f980e44d304468850692f1c957b2f
Daniel Leśniewicz <rootuss@op.pl>
2019-05-08 12:57:14 +0200

    dodanie test3

--- src/main/java/przykladowa_klasa.java
+++ src/main/java/przykladowa_klasa.java
@@ -3,5 +3,6 @@ public class przykladowa_klasa {
     public static void main(String[] args) {
         System.out.println("Test Githuba nr 1");
         System.out.println("Test Githuba nr 2 - online ze strony github.com ");
+        System.out.println("Test Githuba nr 3 środa ");
     }
 }
