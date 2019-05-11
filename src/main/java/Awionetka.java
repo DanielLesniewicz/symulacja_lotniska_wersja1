3c4e522be37a6d76318550888fa1a00d92436b53
Daniel Leśniewicz <rootuss@op.pl>
2019-05-11 12:18:57 +0200

    dodanie awionetki

--- src/main/java/Awionetka.java
+++ src/main/java/Awionetka.java
@@ -1,7 +1,43 @@
-public class Awionetka {
-    private int iloscPasazerow;
+public class Awionetka extends Samolot {
 
-    public Awionetka()
+    private int iloscPasazerow;
+    private Zbiornik zbiornik;
+
+    //konstruktor
+    public Awionetka(String nazwa, int rodzajPasa)
+    {
+        super(nazwa, 3);           // wywołanie konstruktora z klasy nadrzędnej
+        iloscPasazerow = 4;                 // domyślna wartość dla każdego samolotu
+        setCzasZaladunku(300);             // domyślna wartość dla każdego samolotu
+        setCzasTankowania(100);            // domyślna wartość dla każdego samolotu
+        zbiornik = new Zbiornik(100);      // ustawienie domyslnej pojemnosci zbiornika
+    }
+
+    @Override
+    public int ileMiejsc()
+    {
+        return iloscPasazerow;
+    }
+
+    @Override
+    public void zatankuj()
+    {
+
+    }
+    @Override
+    public void ladowanie()
+    {
+
+    }
+
+    @Override
+    public void odlot()
+    {
+
+    }
+
+    @Override
+    public void odstawMagazyn()
     {
 
     }
--- src/main/java/przykladowa_klasa.java
+++ src/main/java/przykladowa_klasa.java
@@ -1,15 +1,6 @@
 public class przykladowa_klasa {
     public static void main(String[] args) {
-        System.out.println("Test Githuba nr 1");
-        System.out.println("Test Githuba nr 2 - online ze strony github.com ");
-        System.out.println("Test Githuba nr 3 środa ");
-        System.out.println("kekekoooooooooooooooooooooe");
-
-        System.out.println("wiadomosc do Patryka");
-
-        System.out.println("Odpowiedz dla Daniela xd");
-
-        System.out.println("Odpowiedz do Patryka");
+        System.out.println("abcd");
 
         }
 }
