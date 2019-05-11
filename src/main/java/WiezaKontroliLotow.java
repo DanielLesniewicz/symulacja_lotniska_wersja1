d821df23a7c3aff26b3cd0a5c1bebd0e81bec3b8
Daniel Leśniewicz <rootuss@op.pl>
2019-05-11 16:09:09 +0200

    rozbidowanie switcha

--- src/main/java/WiezaKontroliLotow.java
+++ src/main/java/WiezaKontroliLotow.java
@@ -65,11 +65,12 @@ public class WiezaKontroliLotow {
             System.out.println("Możliwe jest też, że pasy będą zajęte i nie będzie można wylądować");
 
             //tu wyświetlić jakie samoloty mamy do dyspozycji może
-            int wybor=-1;
+            int wybor=-1, wybor_typu=-1;
 
-            for (; ; ) {
+            for (;;) {
 
                 //tu wyświetlić jakie samoloty mamy do dyspozycji może
+                //dodatkowo tez ich właściwości, np ile pasażerów na pokładzie itp
 
                 //dalej menu co chcemy robić z nimi
 
@@ -84,19 +85,73 @@ public class WiezaKontroliLotow {
                 switch (wybor) {
                         case 1: {
                             System.out.println("Wybrano odlot samolotu.");
-                            System.out.println("jakieś instrukcje dla odlotu");
+                            System.out.println("1. Awionetka");
+                            System.out.println("2. Pasazerski");
+                            System.out.println("3. Towarowy");
+
+                            wybor_typu =in.nextInt();
+
+                                switch (wybor_typu){
+                                    case 1: {
+                                        //działania dla awionetki
+                                        System.out.println("jakieś instrukcje dla odlotu");
+                                        break;
+                                    }
+
+                                    case 2: {
+                                        //działania dla pasażerskiego
+                                        System.out.println("jakieś instrukcje dla odlotu");
+                                        break;
+                                    }
+
+                                    case 3: {
+                                        //działania dla towarowego
+                                        System.out.println("jakieś instrukcje dla odlotu");
+                                        break;
+                                    }
+                                }
+                                //jak odlot to sprwdzić czy jest paliwo losując i jeśli nie ma to zatankować np.
+                                // wpuścić ludzi/czy tam towary załadować itp
+
 
                             break;
                         }
                         case 2: {
                             System.out.println("Wybrano przylot samolotu.");
-                            System.out.println("jakieś instrukcje dla przylotu");
+                            System.out.println("1. Awionetka");
+                            System.out.println("2. Pasazerski");
+                            System.out.println("3. Towarowy");
+
+                            wybor_typu =in.nextInt();
+
+                            switch (wybor_typu){
+                                case 1: {
+                                    //działania dla awionetki
+                                    System.out.println("jakieś instrukcje dla przylotu");
+                                    break;
+                                }
+
+                                case 2: {
+                                    //działania dla pasażerskiego
+                                    System.out.println("jakieś instrukcje dla przylotu");
+                                    break;
+                                }
+
+                                case 3: {
+                                    //działania dla towarowego
+                                    System.out.println("jakieś instrukcje dla przylotu");
+                                    break;
+                                }
+                            }
+
+                            //przylatuje to patrzymy czy jest wolny pas
+                            //i ludzie wysiadają, wtedy odpowiednie parametry trzeba wyzerować
 
                             break;
                         }
 
                         case 3: {
-                            System.out.println("KONIEC");
+                            System.out.println("KONIEC SYMULACJI");
                             System.exit( 0); // wywali program z zerem
                             break;
                         }
